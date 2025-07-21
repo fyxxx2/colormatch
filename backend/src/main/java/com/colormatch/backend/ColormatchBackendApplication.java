1f99ac6716ae32010fe08109999fdb5f6c426f47
fyxxx2 <kkdn0118@gmail.com>
2025-07-21 12:40:05 +0900

    초기설정
    Spring boot

---
+++ backend/src/main/java/com/colormatch/backend/ColormatchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.colormatch.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColormatchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColormatchBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/colormatch/backend/ColormatchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colormatch.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColormatchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
